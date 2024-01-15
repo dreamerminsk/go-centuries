1abf3e7c1434f79caf37a9b9819dfc19507a18b1
dreamerminsk <tutmirage@gmail.com>
2024-01-15 20:30:26 +0000

    format go files

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"strconv"
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -21,41 +21,41 @@ func GetPlayer(text string) string {
 	}
 	return player
 }
-func GetCenturies(text string) []int{
-	centuries:= []int{}
-	values :=strings.Split(strings.TrimSpace(text), ",")
+func GetCenturies(text string) []int {
+	centuries := []int{}
+	values := strings.Split(strings.TrimSpace(text), ",")
 	for _, value := range values {
-	century, err := strconv.Atoi(strings.TrimSpace(value))
-if err != nil {
-	if strings.Contains(value, "147") {
-		centuries=append(centuries, 147)
+		century, err := strconv.Atoi(strings.TrimSpace(value))
+		if err != nil {
+			if strings.Contains(value, "147") {
+				centuries = append(centuries, 147)
+			}
+		} else {
+			centuries = append(centuries, century)
 		}
-} else {
-		centuries=append(centuries, century)
-}
 
-}
+	}
 	return centuries
 }
 func main() {
 
 	file, _ := os.Open("./raw/2024 Masters (snooker).wiki")
 	scanner := bufio.NewScanner(file)
-	ps:=0
-	cs:=0
+	ps := 0
+	cs := 0
 	for scanner.Scan() {
 		line := scanner.Text()
 		if strings.HasPrefix(line, "*") {
 			if strings.Contains(line, "{{ndash}}") {
 				parts := strings.Split(line, "{{ndash}}")
 				centuries := GetCenturies(parts[0])
-				player:=GetPlayer(parts[1])
-				ps = ps+1
-				cs=cs+len(centuries)
+				player := GetPlayer(parts[1])
+				ps = ps + 1
+				cs = cs + len(centuries)
 				fmt.Println(player, len(centuries))
-                                 for _, century := range centuries{
-					 fmt.Println("\t", century)
-				 }
+				for _, century := range centuries {
+					fmt.Println("\t", century)
+				}
 			}
 		}
 	}
