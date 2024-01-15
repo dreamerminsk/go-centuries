203ebf5a935186f0d7b56f9c5dc12ce6ede68ae2
micah teeuws <tutmirage@users.noreply.github.com>
2024-01-15 17:35:10 +0000

    format go files

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
+	"bufio"
+	"fmt"
+	"os"
 )
 
 func main() {
 
-     file, _ := os.Open("./raw/2024 Masters (snooker).wiki")
-     scanner := bufio.NewScanner(file)
-     for scanner.Scan() {
-        fmt.Println(scanner.Text())
-     }
+	file, _ := os.Open("./raw/2024 Masters (snooker).wiki")
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
 
 }
